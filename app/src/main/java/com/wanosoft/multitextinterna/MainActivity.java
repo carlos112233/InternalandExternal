072eb0b2a62459ae47aeb6c19622ee436b0783fa
upam <carlos.araiza#>
2017-02-16 14:32:16 -0600

    Int&Ext

---
+++ app/src/main/java/com/wanosoft/multitextinterna/MainActivity.java
@@ -0,0 +1,103 @@
+package com.wanosoft.multitextinterna;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+    EditText et1,et2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et1=(EditText)findViewById(R.id.et1);
+        et2=(EditText)findViewById(R.id.et2);
+        String[] archivos = fileList();
+
+        if (existe(archivos, "notas.txt"))
+            try {
+                InputStreamReader archivo = new InputStreamReader(
+                        openFileInput("notas.txt"));
+                BufferedReader br = new BufferedReader(archivo);
+                String linea = br.readLine();
+                String todo = "";
+                while (linea != null) {
+                    todo = todo + linea + "\n";
+                    linea = br.readLine();
+                }
+                br.close();
+                archivo.close();
+                et1.setText(todo);
+            } catch (IOException e) {
+            }
+    }
+
+    private boolean existe(String[] archivos, String archbusca) {
+        for (int f = 0; f < archivos.length; f++)
+            if (archbusca.equals(archivos[f]))
+                return true;
+        return false;
+    }
+
+    public void grabar(View v) {
+        try {
+            OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput(
+                    "notas.txt", Activity.MODE_PRIVATE));
+            archivo.write(et1.getText().toString());
+            archivo.flush();
+            archivo.close();
+        } catch (IOException e) {
+        }
+        Toast t = Toast.makeText(this, "Los datos fueron grabados",
+                Toast.LENGTH_SHORT);
+        t.show();
+        finish();
+    }
+
+    public void recuperar(View v) {
+        String nomarchivo=et1.getText().toString();
+        nomarchivo=nomarchivo.replace('/','-');
+        boolean enco=false;
+        String[] archivos = fileList();
+        for (int f = 0; f < archivos.length; f++)
+            if (nomarchivo.equals(archivos[f]))
+                enco= true;
+        if (enco==true) {
+            try {
+                InputStreamReader archivo = new InputStreamReader(
+                        openFileInput(nomarchivo));
+                BufferedReader br = new BufferedReader(archivo);
+                String linea = br.readLine();
+                String todo = "";
+                while (linea != null) {
+                    todo = todo + linea + "\n";
+                    linea = br.readLine();
+                }
+                br.close();
+                archivo.close();
+                et2.setText(todo);
+            } catch (IOException e) {
+            }
+        } else
+        {
+            Toast.makeText(this,"No hay datos grabados para dicha fecha", Toast.LENGTH_LONG).show();
+            et2.setText("");
+        }
+    }
+
+    public void siguiente(View v){
+        Intent i = new Intent(this, Externa.class);
+        startActivity(i);
+
+
+    }
+}
